1e12a03702ba7b9ceb19ead554800e2d5d35bbee
Theo Lenglart <theo.lenglart.etu@univ-lille.fr>
2023-10-23 15:21:20 +0200

    ajout de la classe Coordinate

---
+++ src/main/java/fr/univlille/sae/Coordinate.java
@@ -0,0 +1,30 @@
+package main.java.fr.univlille.sae;
+
+import fr.univlille.iutinfo.cam.player.perception.ICoordinate;
+
+public class Coordinate implements ICoordinate {
+    protected int row;
+    protected int col;
+
+    public Coordinate(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Coordinate()
+    {
+        this(-1,-1);
+    }
+
+    @Override
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public int getRow() {
+        return row;
+    }
+    
+}

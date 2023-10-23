70b6f814c9907cbb2804697985441483e57c1eb5
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2023-10-23 15:58:15 +0200

    MainView creation

--- src/main/java/fr/univlille/sae/Main.java
+++ src/main/java/fr/univlille/sae/Main.java
@@ -1,5 +1,17 @@
 package fr.univlille.sae;
 
-public class Main {
+import fr.univlille.sae.view.MainView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        new MainView();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }

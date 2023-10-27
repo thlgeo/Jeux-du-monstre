3f724003703f04f4a13f21c5b0df7f6fde9f700c
Theo Lenglart <theo.lenglart.etu@univ-lille.fr>
2023-10-27 13:47:49 +0200

    ajout equals Coordinate

--- src/main/java/fr/univlille/sae/model/Coordinate.java
+++ src/main/java/fr/univlille/sae/model/Coordinate.java
@@ -26,5 +26,32 @@ public class Coordinate implements ICoordinate {
     public int getRow() {
         return row;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + row;
+        result = prime * result + col;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coordinate other = (Coordinate) obj;
+        if (row != other.row)
+            return false;
+        if (col != other.col)
+            return false;
+        return true;
+    }
+
+    
     
 }

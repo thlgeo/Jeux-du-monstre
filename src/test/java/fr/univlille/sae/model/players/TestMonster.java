29ae040e6a5c525232f96cede478c136a0dbb762
Theo Lenglart <theo.lenglart.etu@univ-lille.fr>
2023-11-08 10:55:18 +0100

    maj test canMove Monstre (vert)

--- src/test/java/fr/univlille/sae/model/players/TestMonster.java
+++ src/test/java/fr/univlille/sae/model/players/TestMonster.java
@@ -28,24 +28,37 @@ class TestMonster {
             }
         }
         maze[0][0].setInfo(CellInfo.WALL);
-        maze[1][1].setInfo(CellInfo.WALL);;
+        maze[1][1].setInfo(CellInfo.WALL);
         monstre = new Monster("monstre", maze);
         monstre.setCoordinateMonster(new Coordinate(1,0));
     }
 
-    // @Test 
-    // void testCanMove()
-    // {
-    //     assertTrue(monstre.canMove(new Coordinate(2, 0))); // se déplace vers le bas sur une case vide
-    //     assertTrue(monstre.canMove(new Coordinate(2,1))); // se déplace en diagonal
-    //     assertFalse(monstre.canMove(new Coordinate(1,0))); // se déplace sur lui-même
-    //     assertFalse(monstre.canMove(new Coordinate(0, 0))); // se déplace sur un mur
-    //     monstre.setCoordinateMonster(new Coordinate(3,3));
-    //     assertTrue(monstre.canMove(new Coordinate(3,4))); // se déplace à gauche sur une case vide
-    // }
+    @Test 
+    void testCanMoveDiag()
+    {
+        assertTrue(monstre.canMove(new Coordinate(2, 0),true)); // se déplace vers le bas sur une case vide
+        assertTrue(monstre.canMove(new Coordinate(2,1),true)); // se déplace en diagonal
+        assertFalse(monstre.canMove(new Coordinate(1,0),true)); // se déplace sur lui-même
+        monstre.setCoordinateMonster(new Coordinate(3,3));
+        assertTrue(monstre.canMove(new Coordinate(3,4),true)); // se déplace à gauche sur une case vide
+        assertTrue(monstre.canMove(new Coordinate(3,4),true)); // se déplace à droite sur une case vide
+        assertTrue(monstre.canMove(new Coordinate(2,3),true)); // se déplace vers le haut sur une case vide
+    }
 
     @Test
-    void test_update() {
+    void testCanMoveNotDiag()
+    {
+        assertTrue(monstre.canMove(new Coordinate(2, 0),false)); // se déplace vers le bas sur une case vide
+        assertFalse(monstre.canMove(new Coordinate(2,1),false)); // se déplace en diagonal
+        assertFalse(monstre.canMove(new Coordinate(1,0),false)); // se déplace sur lui-même
+        monstre.setCoordinateMonster(new Coordinate(3,3));
+        assertTrue(monstre.canMove(new Coordinate(3,2),false)); // se déplace à gauche sur une case vide
+        assertTrue(monstre.canMove(new Coordinate(3,4),false)); // se déplace à droite sur une case vide
+        assertTrue(monstre.canMove(new Coordinate(2,3),false)); // se déplace vers le haut sur une case vide
+    }
+
+    @Test
+    void testUpdate() {
         ICoordinate coord = new Coordinate(2, 2);
         CellEvent event = new CellEvent(5, ICellEvent.CellInfo.MONSTER, coord);
         ICoordinate coordH = new Coordinate(0, 0);
